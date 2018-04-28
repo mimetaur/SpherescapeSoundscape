db9c66661e3c6591ae060078c0bc14cef091aa90
Nathan Koch <nathanrkoch@gmail.com>
2018-04-28 10:58:58 -0400

    imported Spherescapes aka Prototype 02

---
+++ Assets/Scripts/Attractor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attractor : MonoBehaviour
+{
+
+    public float attractionRadius = 2;
+    public float attractionForce = 1;
+    public string tagToAttract;
+
+    public void FixedUpdate()
+    {
+        foreach (Collider theCollider in Physics.OverlapSphere(transform.position, attractionRadius))
+        {
+            if (theCollider.tag == tagToAttract)
+            {
+                // calculate direction from target to me
+                Vector3 forceDirection = transform.position - theCollider.transform.position;
+
+                // apply force on target towards me
+                var rb = theCollider.GetComponent<Rigidbody>();
+                if (rb) rb.AddForce(forceDirection.normalized * attractionForce * Time.fixedDeltaTime);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/KillWithinArea.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillWithinArea : MonoBehaviour
+{
+    public string tagToKill;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == tagToKill) Destroy(other.gameObject);
+    }
+}
