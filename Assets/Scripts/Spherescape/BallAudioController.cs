cac7d9ea0cbf631d23634e14cd051e39e8ad787a
Nathan Koch <nathanrkoch@gmail.com>
2018-05-01 09:36:01 -0400

    ball audio controller and basic trigger code

---
+++ Assets/Scripts/Spherescape/BallAudioController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallAudioController : MonoBehaviour
+{
+    Hv_spherescape_AudioLib heavyScript;
+
+    void Start()
+    {
+        heavyScript = GetComponent<Hv_spherescape_AudioLib>();
+    }
+
+    public void Trigger()
+    {
+        heavyScript.SendEvent(Hv_spherescape_AudioLib.Event.Bang);
+    }
+}
---
+++ Assets/Scripts/Spherescape/BallEnter.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallEnter : MonoBehaviour
+{
+
+    public GameObject ballType;
+    public float octave = 0;
+
+    private BallAudioController ballAudioController;
+    private string triggerTag;
+
+    // Use this for initialization
+    void Start()
+    {
+        triggerTag = ballType.tag;
+        ballAudioController = GetComponent<BallAudioController>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == triggerTag)
+        {
+            ballAudioController.Trigger();
+
+            // once I get the PD script wired up to
+            // respond to these params, I'll send this data to
+            // the Audio Controller as well
+
+            // List<float> floatParams = new List<float>();
+            // floatParams.Add(octave);
+
+            // float speed = other.gameObject.GetComponent<Rigidbody>().velocity.magnitude;
+            // floatParams.Add(speed);
+
+            // float size = other.transform.localScale.x;
+            // floatParams.Add(size);
+
+            // float rotation = other.transform.rotation.eulerAngles.z;
+            // floatParams.Add(rotation);
+
+            // floatParams.Add(other.transform.position.x);
+            // floatParams.Add(other.transform.position.y);
+            // floatParams.Add(other.transform.position.z);
+        }
+    }
+}
