2eaa2664768ab0f597b329478da1a2f8a2f0b478
Nathan Koch <nathanrkoch@gmail.com>
2018-05-01 11:40:29 -0400

    switched to per-ball audio source. adds dimensionality

--- Assets/Scripts/Spherescape/BallEnter.cs
+++ Assets/Scripts/Spherescape/BallEnter.cs
@@ -4,45 +4,36 @@ using UnityEngine;
 
 public class BallEnter : MonoBehaviour
 {
-
-    public GameObject ballType;
-    public float octave = 0;
-
     private BallAudioController ballAudioController;
-    private string triggerTag;
 
     // Use this for initialization
     void Start()
     {
-        triggerTag = ballType.tag;
         ballAudioController = GetComponent<BallAudioController>();
     }
 
-    void OnTriggerEnter(Collider other)
+    public void EnteredNet()
     {
-        if (other.tag == triggerTag)
-        {
-            ballAudioController.Trigger();
+        ballAudioController.Trigger();
 
-            // once I get the PD script wired up to
-            // respond to these params, I'll send this data to
-            // the Audio Controller as well
+        // once I get the PD script wired up to
+        // respond to these params, I'll send this data to
+        // the Audio Controller as well
 
-            // List<float> floatParams = new List<float>();
-            // floatParams.Add(octave);
+        // List<float> floatParams = new List<float>();
+        // floatParams.Add(octave);
 
-            // float speed = other.gameObject.GetComponent<Rigidbody>().velocity.magnitude;
-            // floatParams.Add(speed);
+        // float speed = other.gameObject.GetComponent<Rigidbody>().velocity.magnitude;
+        // floatParams.Add(speed);
 
-            // float size = other.transform.localScale.x;
-            // floatParams.Add(size);
+        // float size = other.transform.localScale.x;
+        // floatParams.Add(size);
 
-            // float rotation = other.transform.rotation.eulerAngles.z;
-            // floatParams.Add(rotation);
+        // float rotation = other.transform.rotation.eulerAngles.z;
+        // floatParams.Add(rotation);
 
-            // floatParams.Add(other.transform.position.x);
-            // floatParams.Add(other.transform.position.y);
-            // floatParams.Add(other.transform.position.z);
-        }
+        // floatParams.Add(other.transform.position.x);
+        // floatParams.Add(other.transform.position.y);
+        // floatParams.Add(other.transform.position.z);
     }
 }
---
+++ Assets/Scripts/Spherescape/BallEnteredNet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallEnteredNet : MonoBehaviour
+{
+
+
+    public GameObject ballType;
+    public float octave = 0;
+    private string triggerTag;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        triggerTag = ballType.tag;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == triggerTag)
+        {
+            BallEnter ballScript = other.GetComponent<BallEnter>();
+            ballScript.EnteredNet();
+        }
+    }
+}
