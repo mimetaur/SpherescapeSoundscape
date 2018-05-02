02c8ba05dc0ab077d17ad7b7bb57754bea2e2c72
Nathan Koch <nathanrkoch@gmail.com>
2018-05-02 19:10:11 -0400

    repel balls go emissive when they're hearding. all balls die after a minute

---
+++ Assets/Scripts/Spherescape/RepelBrightnessController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepelBrightnessController : MonoBehaviour
+{
+
+    public Color brightColor;
+    public float brightnessFactor = 3.0f;
+    private Repel repel;
+    private Material myMaterial;
+
+    // Use this for initialization
+    void Start()
+    {
+        repel = GetComponent<Repel>();
+        myMaterial = GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float amount = GameUtils.Map(repel.numberOfBallsRepelling, 0, repel.expectedMaxNumberOfBallsRepelling, 0.0f, 1.0f);
+        myMaterial.color = Color.Lerp(myMaterial.color, brightColor * brightnessFactor, amount);
+    }
+}
