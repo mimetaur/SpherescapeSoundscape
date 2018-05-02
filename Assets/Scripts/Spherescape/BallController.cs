22fcb595c2325b481c3b448a848885cb7178c48d
Nathan Koch <nathanrkoch@gmail.com>
2018-05-02 19:10:11 -0400

    repel balls go emissive when they're hearding. all balls die after a minute

---
+++ Assets/Scripts/Spherescape/BallController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public float lifespan = 1.0f;
+    private float age;
+
+    // Use this for initialization
+    void Start()
+    {
+        age = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        age += Time.deltaTime;
+        if (age > lifespan)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
