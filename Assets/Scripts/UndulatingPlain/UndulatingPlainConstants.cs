a11056d670dcdf3ed8c55a230953bafda562e9ad
Nathan Koch <nathanrkoch@gmail.com>
2018-05-03 19:24:16 -0400

    actual improvements

--- Assets/Scripts/UndulatingPlain/UndulatingPlainConstants.cs
+++ Assets/Scripts/UndulatingPlain/UndulatingPlainConstants.cs
@@ -6,6 +6,7 @@ public static class UndulatingPlainConstants
 {
     public const string metallicSubstancePropertyName = "overallMetallicness";
     public const string iceSubstancePropertyName = "ice_vs_fire";
+    public const string fiberVsFractalPropertyName = "fiber_vs_fractal";
 
     public const float metalLerpValueRange = 0.5f;
     public const float metalLerpValueOffset = 0.5f;
@@ -15,4 +16,7 @@ public static class UndulatingPlainConstants
 
     public const float echoLerpValueRange = 1.0f;
     public const float echoLerpValueOffset = 0f;
+
+    public const float fiberLerpValueRange = 0.05f;
+    public const float fiberLerpValueOffset = 0.6975f;
 }
