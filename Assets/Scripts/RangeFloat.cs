790c809782a4abf0728077a41367cd4d7d2dce1d
Nathan Koch <nathanrkoch@gmail.com>
2018-05-03 08:52:00 -0400

    more musical variety in individual sounds. using range classes to organize cases where I want each prefab to have a different value

---
+++ Assets/Scripts/RangeFloat.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class RangeFloat
+{
+    public float lowBound;
+    public float highBound;
+
+    public RangeFloat(float lowBound_, float highBound_)
+    {
+        lowBound = lowBound_;
+        highBound = highBound_;
+    }
+
+    public float GetRandomValueFromRange()
+    {
+        return Mathf.Floor(Random.Range(lowBound, highBound));
+    }
+}
--- Assets/Scripts/Spherescape/RepelAudioController.cs
+++ Assets/Scripts/Spherescape/RepelAudioController.cs
@@ -5,28 +5,41 @@ using UnityEngine;
 public class RepelAudioController : MonoBehaviour
 {
     public int currentNote = 64;
+    public RangeFloat updateRateRange = new RangeFloat(2f, 6);
+    public RangeFloat intensityToFilterAmountRange = new RangeFloat(70f, 80f); // originally 76.5
+    public RangeFloat intensityToLfoSpeedAmountRange = new RangeFloat(30f, 50f); // originally 40
 
-    [Range(1, 20)]
-    public float updateRateLow = 2.0f;
-    [Range(1, 20)]
-    public float updateRateHigh = 6.0f;
-
-    private float updateRate;
     private BallMusicalNotes ballMusicalNotes;
     private Hv_spherescapeRepel_AudioLib heavyScript;
     private Repel repel;
 
+    private float updateRate;
+    private float intensityToFilterAmount;
+    private float intensityToLfoSpeedAmount;
+
     // Use this for initialization
     void Start()
     {
-        updateRate = Random.Range(updateRateLow, updateRateHigh);
+        updateRate = updateRateRange.GetRandomValueFromRange();
+        intensityToFilterAmount = intensityToFilterAmountRange.GetRandomValueFromRange();
+        intensityToLfoSpeedAmount = intensityToLfoSpeedAmountRange.GetRandomValueFromRange();
 
         ballMusicalNotes = GameObject.Find("GameManager").GetComponent<BallMusicalNotes>();
         repel = GetComponent<Repel>();
+
         heavyScript = GetComponent<Hv_spherescapeRepel_AudioLib>();
+
+        // each sphere has a different note, set at startup in the spawn script via BallMusicalNotes class
         heavyScript.SetFloatParameter(Hv_spherescapeRepel_AudioLib.Parameter.Noteasnumber, (float)currentNote);
 
+        // each repel sphere should have slightly different characteristics
+        // effected differently by intensity parameter
+        heavyScript.SetFloatParameter(Hv_spherescapeRepel_AudioLib.Parameter.Intensitytofilteramount, intensityToFilterAmount);
+
+        heavyScript.SetFloatParameter(Hv_spherescapeRepel_AudioLib.Parameter.Intensitytolfospeed, intensityToLfoSpeedAmount);
+
         // this parameter and the the rate of update are in sync
+        // makes for smoother parameter changes - using hv.line~ in PD
         heavyScript.SetFloatParameter(Hv_spherescapeRepel_AudioLib.Parameter.Intensityfadetime, updateRate);
 
         InvokeRepeating("UpdateCount", updateRate, updateRate);
@@ -34,7 +47,7 @@ public class RepelAudioController : MonoBehaviour
 
     void UpdateCount()
     {
-        float intensityAmount = GameUtils.Map((float)repel.numberOfBallsRepelling, 0, repel.expectedMaxNumberOfBallsRepelling, 0, 127);
+        float intensityAmount = GameUtils.Map((float)repel.numberOfBallsRepelling, 0, repel.expectedMaxNumberOfBallsRepelling, SpherescapeConstants.MinIntensity, SpherescapeConstants.MaxIntensity);
         heavyScript.SetFloatParameter(Hv_spherescapeRepel_AudioLib.Parameter.Intensity, intensityAmount);
     }
 }
