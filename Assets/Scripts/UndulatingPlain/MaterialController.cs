bb09fa6367e903df979f66751fc9e5409f4ceff3
Nathan Koch <nathanrkoch@gmail.com>
2018-05-03 15:12:10 -0400

    animating plain

---
+++ Assets/Scripts/UndulatingPlain/MaterialController.cs
@@ -0,0 +1,35 @@
+﻿#pragma warning disable 0618
+// TODO upgrade project to 2018.1 and Substance Designer asset
+// yes Unity I KNOW ProceduralMaterials are obsolete
+// but you have given me no alternative in Unity 2017
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialController : MonoBehaviour
+{
+    private PlainManager plainManager;
+    private Renderer rend;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        plainManager = GameObject.Find("PlainManager").GetComponent<PlainManager>();
+    }
+
+    void Update()
+    {
+        ProceduralMaterial substance = rend.sharedMaterial as ProceduralMaterial;
+        if (substance)
+        {
+            float metalLerp = Mathf.PingPong(Time.time * 2 / plainManager.metallicCycleTime, 0.5f) + 0.5f;
+            substance.SetProceduralFloat(UndulatingPlainConstants.metallicSubstancePropertyName, metalLerp);
+
+            float iceLerp = Mathf.PingPong(Time.time * 2 / plainManager.iceCycleTime, 1.0f);
+            substance.SetProceduralFloat(UndulatingPlainConstants.iceSubstancePropertyName, iceLerp);
+
+            substance.RebuildTextures();
+        }
+    }
+}
---
+++ Assets/Scripts/UndulatingPlain/UndulatingPlainConstants.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class UndulatingPlainConstants
+{
+    public const string metallicSubstancePropertyName = "overallMetallicness";
+    public const string iceSubstancePropertyName = "ice_vs_fire";
+}
