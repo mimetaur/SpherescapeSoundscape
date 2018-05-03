c798552918c51cfd66d1d55b68ebe1c595a2868b
Nathan Koch <nathanrkoch@gmail.com>
2018-05-03 17:42:22 -0400

    using ice lerp to control cutoff/reso

--- Assets/Scripts/UndulatingPlain/UndulatingAudioController.cs
+++ Assets/Scripts/UndulatingPlain/UndulatingAudioController.cs
@@ -1,9 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class UndulatingAudioController : MonoBehaviour
 {
+    public AudioMixer mixer;
+    public RangeFloat cutoffRange = new RangeFloat(500f, 1200f);
+    public RangeFloat resonanceRange = new RangeFloat(1f, 2.5f);
+
     private Hv_prototype03_AudioLib heavyScript;
     private PlainManager plainManager;
 
@@ -25,13 +30,18 @@ public class UndulatingAudioController : MonoBehaviour
 
     private void Update()
     {
-        float metalLerp = plainManager.GetMetalLerp();
-        float metalParam = GameUtils.Map(metalLerp, 0.5f, 1f, 0f, 127f);
-        // setfloatparameter for heavy
+        float longSlowLerp = plainManager.GetIceLerp();
 
-        float iceLerp = plainManager.GetIceLerp();
-        float iceParam = GameUtils.Map(iceLerp, 0f, 1f, 0f, 127f);
-        // setfloatparameter for heavy
+        float cutoffParam = GameUtils.Map(longSlowLerp, 0f, 1f, cutoffRange.Low(), cutoffRange.High());
+        mixer.SetFloat("LowpassCutoff", cutoffParam);
+
+        float resonanceParam = GameUtils.Map(longSlowLerp, 0f, 1f, resonanceRange.Low(), resonanceRange.High());
+        mixer.SetFloat("LowpassResonance", resonanceParam);
+
+        float metalLerp = plainManager.GetMetalLerp();
+        // float cutoffParam = GameUtils.Map(iceLerp, 0f, 1f, 500f, 1200.0f);
+        // float metalParam = GameUtils.Map(metalLerp, 0.5f, 1f, 0f, 127f);
+        // float cutoffParam = GameUtils.Map(metalLerp, 0.5f, 1f, 0f, 1200.0f);
     }
 
     public void SetMetallicValue(float newMetallicValue)
