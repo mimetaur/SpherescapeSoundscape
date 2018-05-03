8bf80e9d32c4d33025215b1024803e69f28ccae1
Nathan Koch <nathanrkoch@gmail.com>
2018-05-03 15:12:10 -0400

    animating plain

---
+++ Assets/Scripts/UndulatingPlain/PlainManager.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlainManager : MonoBehaviour
+{
+    public float metallicCycleTime = 10f;
+    public float iceCycleTime = 30f;
+}
