80c3e741c8d98997ae2f2051cc5983005309bc59
Nathan Koch <nathanrkoch@gmail.com>
2018-05-03 08:52:00 -0400

    more musical variety in individual sounds. using range classes to organize cases where I want each prefab to have a different value

---
+++ Assets/Scripts/RangeInt.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class RangeInt
+{
+    public int lowBound;
+    public int highBound;
+
+    public RangeInt(int lowBound_, int highBound_)
+    {
+        lowBound = lowBound_;
+        highBound = highBound_;
+    }
+
+    public int GetRandomValueFromRange()
+    {
+        return (int)Mathf.Floor(Random.Range(lowBound, highBound));
+    }
+}
---
+++ Assets/Scripts/Spherescape/SpherescapeConstants.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class SpherescapeConstants
+{
+    public const float MinIntensity = 0f;
+    public const float MaxIntensity = 127.0f;
+}
