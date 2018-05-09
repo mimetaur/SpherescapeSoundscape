45df9f41547812894c6041ee29e8b6a76ec71d7c
Nathan Koch <nathanrkoch@gmail.com>
2018-05-09 14:20:58 -0400

    initial picker screen

---
+++ Assets/Scripts/IntroScenePicker.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroScenePicker : MonoBehaviour
+{
+
+    public void ChangeToUndulatingPlain()
+    {
+        SceneManager.LoadScene("UndulatingPlain");
+    }
+
+    public void ChangeToSpherescape()
+    {
+        SceneManager.LoadScene("Spherescape");
+    }
+}
